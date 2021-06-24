978a060b32bb707e135d63d62e0c26c072e2d4d0
mark <mcgaritym@gmail.com>
2021-06-23 21:34:14 -0400

    "updated w/ AWS storage"

--- src/cloud/config_cloud.py
+++ src/cloud/config_cloud.py
@@ -7,14 +7,14 @@ Created on Thu May 27 22:16:52 2021
 """
 
 # credentials
-user = ''
-pwd = ""
-host = ''
+user = 'admin'
+pwd = "Nalgene09!"
+host = 'bikeshare-database-1.cj4j9csypvlc.us-east-2.rds.amazonaws.com'
 port = int(3306)
-db = ''
+db = 'Bikeshare'
 
 # s3 credentials
-service_name = ''
-region_name = ''
-aws_access_key_id = ''
-aws_secret_access_key = ''
+service_name = 's3'
+region_name = 'us-east-2'
+aws_access_key_id = 'AKIAUT5FFG265JITH5MC'
+aws_secret_access_key = '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'
